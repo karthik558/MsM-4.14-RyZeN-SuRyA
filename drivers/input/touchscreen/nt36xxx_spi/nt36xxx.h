11faa349fd0f67298aa8d784f2a0d31b6492a734
wangjian2 <wangjian2@longcheer.com>
2020-04-28 19:02:13 +0800

    [M700][tp][HMI_M700_A01-2]pocket mode
    [Rootcause]:The project requirements
    [Solution]:update code
    [SideEffect]:no
    [Others]:no
    
    Change-Id: I4fc7a19001cd36eae7ec5c8e9b1a1afe88405663
    Signed-off-by: wangjian2 <wangjian2@longcheer.com>

--- drivers/input/touchscreen/nt36xxx_spi/nt36xxx.h
+++ drivers/input/touchscreen/nt36xxx_spi/nt36xxx.h
@@ -99,7 +99,7 @@ extern const uint16_t touch_key_array[TOUCH_KEY_NUM];
 #if WAKEUP_GESTURE
 extern const uint16_t gesture_key_array[];
 #endif
-#define LCT_TP_PALM_EN 0
+#define LCT_TP_PALM_EN		1
 #define BOOT_UPDATE_FIRMWARE 1
 #define FIRMWARE_NAME_LEN    256
 #define BOOT_UPDATE_FIRMWARE_NAME         "novatek_ts_fw.bin"
